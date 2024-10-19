8147f601e1b8ab96c9fefb732ec8e1b17c3b0921
Afrah Nakanwagi <anaiyosh@gmail.com>
2024-10-19 23:42:09 +0300

    included tests for other multiplications

--- test/multiplication.js
+++ test/multiplication.js
@@ -1,6 +1,6 @@
-// test/multiply.test.js
+// test/multiplication.test.mjs
 
-const { expect } = require('chai');
+import { expect } from 'chai';
 
 // Simple multiplication function
 function multiply(a, b) {
@@ -11,5 +11,25 @@ function multiply(a, b) {
 describe('Multiplication Function', () => {
     it('should return 1 when multiplying 1 by 1', () => {
         expect(multiply(1, 1)).to.equal(1);
+    });
+
+    it('should return 4 when multiplying 2 by 2', () => {
+        expect(multiply(2, 2)).to.equal(4);
+    });
+
+    it('should return 9 when multiplying 3 by 3', () => {
+        expect(multiply(3, 3)).to.equal(9);
+    });
+
+    it('should return 16 when multiplying 4 by 4', () => {
+        expect(multiply(4, 4)).to.equal(16);
+    });
+
+    it('should return 575 when multiplying 23 by 25', () => {
+        expect(multiply(23, 25)).to.equal(575);
+    });
+
+    it('should return 575 when multiplying 25 by 23', () => {
+        expect(multiply(25, 23)).to.equal(575);
     });
 });
